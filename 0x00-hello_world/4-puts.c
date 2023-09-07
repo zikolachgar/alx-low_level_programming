0e4dc001d53b06e02e563e8290afdca68d872fb2
zikolachgar <zakarialachgar32@gmail.com>
2023-09-07 14:29:22 -0700

    task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - a program that print a line using the function puts
- * Return : 0
- * */
+ * Return : 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
