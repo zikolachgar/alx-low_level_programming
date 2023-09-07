42cb035f4465ce907508f99cc1d0d98b279fa13a
zikolachgar <zakarialachgar32@gmail.com>
2023-09-07 15:01:53 -0700

    task 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using printf
-function
+ * main - A program that print a line using printf function
  * Return: 0 (Success)
  */
 int main(void)
